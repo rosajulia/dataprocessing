c379f5fa0a45c94dcb899b9ba17cf4040ad04037
juliajelgerhuis <36192695+juliajelgerhuis@users.noreply.github.com>
2018-04-25 17:42:13 +0200

    update homework

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -7,5 +7,5 @@ with open("donor_data.csv") as file_donor:
     donor_data = json.dumps([row for row in reader])
 
 # write dict to JSON
-json_file = open("donor_data.json", 'w')
+json_file = open("donor_data.json", "w")
 json_file.write(donor_data)
