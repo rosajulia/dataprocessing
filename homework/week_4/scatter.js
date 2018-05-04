eb4d94f03cf9804b244254cc2cd4e042444d1ef5
juliajelgerhuis <36192695+juliajelgerhuis@users.noreply.github.com>
2018-05-04 19:25:42 +0200

    finished

--- homework/week_4/scatter.js
+++ homework/week_4/scatter.js
@@ -6,7 +6,7 @@ Minor programmeren - dataprocessing
 
 window.onload = function loadData() {
   console.log('Yes, you can!')
-  var datasetAll = "http://stats.oecd.org/SDMX-JSON/data/CSPCUBE/NATINCCAP_T1+HOURSWKD_T1+LIFEEXPY_G1.DNK+FIN+ITA+LVA+NLD+POL+ESP+CHE+GBR+USA/all?startTime=2011&endTime=2015&dimensionAtObservation=allDimensions"
+  var datasetAll = "https://stats.oecd.org/SDMX-JSON/data/CSPCUBE/NATINCCAP_T1+HOURSWKD_T1+LIFEEXPY_G1.DNK+FIN+ITA+LVA+NLD+POL+ESP+GBR+USA/all?startTime=2011&endTime=2015&dimensionAtObservation=allDimensions"
 
   d3.queue()
     .defer(d3.request, datasetAll)
@@ -15,6 +15,9 @@ window.onload = function loadData() {
   function getData(error, response) {
     if (error) throw error;
 
+    // call dropdown menu
+    dropdownMenu();
+
     // use response to get data
     var data = JSON.parse(response[0].responseText)
     var datapoints = data.dataSets[0].observations
@@ -22,7 +25,7 @@ window.onload = function loadData() {
     var countries = []
     
     // add country names to array
-    for (let i = 0; i < 10; i++){
+    for (let i = 0; i < 9; i++){
       countries.push(data.structure.dimensions.observation[1].values[i].name)
     };
 
@@ -33,7 +36,7 @@ window.onload = function loadData() {
 
     // loop over datapoints
     for (let i = 0; i < 3; i++){
-      for (let j = 0; j < 10; j++){
+      for (let j = 0; j < 9; j++){
         for (let k = 0; k < 5; k++){
           var key = i + ":" + j + ":" + k
           if (i == 0){
@@ -53,7 +56,7 @@ window.onload = function loadData() {
     var allCountries = []
 
     // give country values
-    for (let i = 0; i < 10; i++){
+    for (let i = 0; i < 9; i++){
       var countryObj = {
         "NAME": countries[i]
       }
@@ -74,16 +77,35 @@ window.onload = function loadData() {
       
       allCountries.push(countryObj)
     }
+    
+    function dropdownMenu(){
+      var data = [0, 1, 2, 3, 4];
 
-    console.log(allCountries)
+      var select = d3.select("body")
+                    .append("select")
+                    .attr("class","select")
+                    .on("change", onchange)
 
+      var options = select.selectAll("option")
+                          .data(data)
+                          .enter()
+                          .append("option")
+                          .text(function (d) { return d; });
+
+      function onchange() {
+        selectValue = d3.select("select").property("value")
+        console.log(selectValue)
+        drawGraph(allCountries, selectValue)
+      };
+    };   
     /* Create SVG and its elements */
     // height, width, and margins
-    totalHeight = 600;
+    function drawGraph(allCountries, years) {
+    totalHeight = 700;
     totalWidth = 900;
     padding = 20;
     
-    margin = {"left": 90, "right": 20, "top": 60, "bottom": 50};
+    margin = {"left": 90, "right": 40, "top": 80, "bottom": 50};
 
     graphHeight = totalHeight - margin.top - margin.bottom;
     graphWidth = totalWidth - margin.left - margin.right;
@@ -92,20 +114,24 @@ window.onload = function loadData() {
     maxGNI = [];
     minLIFE = [];
     maxLIFE = [];
+    minHOURS = [];
+    maxHOURS = [];
 
     // domains and ranges
-    for (let i = 0; i < 10; i++){
+    for (let i = 0; i < 9; i++){
       minGNI.push(Math.min.apply(null, allCountries[i].GNI))
       minLIFE.push(Math.min.apply(null, allCountries[i].LIFE))
+      minHOURS.push(Math.min.apply(null, allCountries[i].HOURS))
       maxGNI.push(Math.max.apply(null, allCountries[i].GNI))
       maxLIFE.push(Math.max.apply(null, allCountries[i].LIFE))
+      maxHOURS.push(Math.max.apply(null, allCountries[i].HOURS))
     }
 
     var domainX = [Math.floor(Math.min.apply(null, minLIFE)), Math.ceil(Math.max.apply(null, maxLIFE))] 
-    var domainY = [Math.floor(Math.min.apply(null, minGNI)), Math.ceil(Math.max.apply(null, maxGNI))] 
+    var domainY = [Math.floor(Math.min.apply(null, minGNI)), 75000] 
 
     var rangeX = [margin.left, graphWidth];
-    var rangeY = [graphHeight - margin.top, 0];
+    var rangeY = [graphHeight - margin.top, margin.bottom];
 
     // x and y scales
     var xScale = d3.scaleLinear()
@@ -116,6 +142,8 @@ window.onload = function loadData() {
                    .range(rangeY)
                    .domain(domainY)
 
+    d3.select("svg").remove();
+    
     var svg = d3.select("body")
                 .append("svg")
                 .attr("width", totalWidth)
@@ -155,16 +183,47 @@ window.onload = function loadData() {
        .text("GNI (dollars)");   
     
     // colors for dots
-    var color = d3.scaleOrdinal().range(["#ECDB54", "#E94B3C", "#6F9FD8", "#944743", "#DBB1CD",
-                                            "#EC9787", "#00A591", "#6B5B95", "#6C4F3D", "#FF1A1A"]);
+    var color = d3.scaleOrdinal().range(['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99',
+                                            '#e31a1c','#fdbf6f','#ff7f00','#cab2d6']);
+    
+
+    var legend = svg.selectAll(".legend")
+                    .data(countries)
+                    .enter()
+                    .append("g")
+                    .classed("legend", true)
+                    .attr("transform", function(d, i) {
+                        return "translate(0," + i * 20 + ")";
+                    })
+                   
+    legend.append("rect")
+          .attr("x", graphWidth + 20)
+          .attr("width", 12)
+          .attr("height", 12)
+          .style("fill", color);  
+    
+    legend.append("text")
+          .attr("class", "text-legend")
+          .attr("x", graphWidth + 40)
+          .attr("y", 7)
+          .attr("dy", ".35em")
+          .style("text-anchor", "begin")
+          .text(function(d, i){
+            return allCountries[i].NAME
+          });                                      
+
+    // sizes for dots
+    var sizes = [5, 8, 11];
     
     // create tip for tooltip
     var tip = d3.tip()
                 .attr('class', 'd3-tip')
                 .offset([-5, 0])
                 .html(function(d) {
-                    return "<strong>Country:</strong> <span style='color:red'>" + d.NAME + "</span>";
-    })                                        
+                  var tipText = "<strong>Country:</strong> <span style='color:red'>" + d.NAME + "</span>" + "<br>" + 
+                                    "<strong>Hours worked:</strong> <span style='color:red'>" + d.HOURS[years] + "</span>" + "<br>";
+                    return tipText;
+                })                                        
 
     // call tip in svg
     svg.call(tip);
@@ -175,16 +234,27 @@ window.onload = function loadData() {
        .enter()
        .append("circle")
        .attr("cx", function(d) {
-        return xScale(d.LIFE[0]);
+        return xScale(d.LIFE[years]);
        })
        .attr("cy", function(d) {
-              return yScale(d.GNI[0]);
+        return yScale(d.GNI[years]);
        })
-       .attr("r", 5)
+       .attr("r", function(d){
+         if (d.HOURS[years] <= 1500){
+           return sizes[0];
+         }
+         if (d.HOURS[years] >= 1800){
+           return sizes[2];
+         }
+         else{
+           return sizes[1];
+         }
+       })
        .attr("fill", function(d, i) {
         return color(i);
        })
        .on('mouseover', tip.show)
-       .on('mouseout', tip.hide);
+       .on('mouseout', tip.hide);  
   };
+};
 };  
